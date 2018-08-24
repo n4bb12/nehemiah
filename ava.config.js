5832dcc3a3fe52233bd507cdaa5c08347a50e448
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-08-24 02:59:02 +0200

    Jest -> Ava

---
+++ ava.config.js
@@ -0,0 +1,12 @@
+export default {
+  cache: true,
+  compileEnhancements: false,
+  concurrency: 5,
+  extensions: ["ts"],
+  failFast: false,
+  failWithoutAssertions: false,
+  files: ["test/**/*.ts"],
+  require: ["ts-node/register"],
+  sources: ["**/*.ts"],
+  verbose: true,
+}
