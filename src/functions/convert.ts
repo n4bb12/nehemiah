f65bfe23f89b4da91f69d9daba2632bb998b4b50
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-09-08 15:41:06 +0200

    Add noop default converter

--- src/functions/convert.ts
+++ src/functions/convert.ts
@@ -5,19 +5,24 @@ import { Converter } from "../types"
 const converters: {
   [key: string]: Converter,
 } = {
+  noop: {
+    parse: (input: any) => input,
+    stringify: (input: any) => input,
+  },
   json: {
     parse: input => JSON.parse(input),
     stringify: input => JSON.stringify(input),
   },
 }
 
-export function getConverter<T>(filename: string): Converter {
+export function getConverter(filename: string): Converter {
   const ext = path.extname(filename) || filename
   if (ext.length > 1) {
     const converter = converters[ext.substr(1)]
     if (converter) {
       return converter
     }
+    return converters.noop
   }
   throw new Error("Converter not found: " + filename)
 }
