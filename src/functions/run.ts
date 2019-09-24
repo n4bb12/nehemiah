7d197c7935901093e95badf0cba9cae6599b95d5
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-09-24 11:21:35 +0200

    Update deps

--- src/functions/run.ts
+++ src/functions/run.ts
@@ -1,17 +1,24 @@
-import execa, { ExecaReturns, Options as ExecaOptions } from "execa"
+import execa, { ExecaReturnValue, Options as ExecaOptions } from "execa"
 import { merge } from "lodash"
 
 import { Context } from "../types"
 
-export type RunResult = Promise<ExecaReturns>
 export type RunOptions = ExecaOptions
+export type RunResult = Promise<ExecaReturnValue<string>>
 
 const defaultOptions: execa.Options = {
   stdio: "inherit",
 }
 
 export async function runCommand(context: Context, cmd: string, options?: RunOptions): RunResult {
-  const execaOptions = merge({ cwd: context.cwd }, defaultOptions, options)
+  const execaOptions = merge(
+    {
+      cwd: context.cwd,
+      shell: true,
+    },
+    defaultOptions,
+    options,
+  )
   const command = `sh -c \"${cmd.replace(/"/g, `\\\"`)}\"`
-  return execa.shell(command, execaOptions)
+  return execa(command, execaOptions)
 }
